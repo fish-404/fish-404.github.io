52fb9fa30dfee371a7bdff4bf08a7b76c56d66d4
fish-404 <fish404hsif@gmail.com>
2019-02-27 21:32:17 +0800

    Site updated: 2019-02-27 21:31:10

--- js/src/motion.js
+++ js/src/motion.js
@@ -1,6 +1,21 @@
 /* global NexT: true */
 
 $(document).ready(function () {
+  window.onload = function(){
+	  val bwol = documnet.body.offsetWidth;
+	  if (bwol < 975) {
+		  $('aside#sidebar').css("display", "none");
+		  $('body').css("paddingLeft","0px");
+	  }
+  }
+
+  window.onresize = function() {
+	  var bowos = document.body.offsetWidth;
+	  bwos < 975 $('body').velocity('stop').velocity({paddingLeft:0}, 0};
+	  if ($('aside#sidebar').css('display') != 'none' && $('aside#sidebar').css('width') != '0px')
+		$('body').velocity('stop').velocity({paddingLeft: 350}, 0);
+  }
+
   NexT.motion = {};
 
   var sidebarToggleLines = {
